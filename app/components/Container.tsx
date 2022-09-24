504bff681299807dbc9df683ccdb62de8898421b
Ael Festijo <aelfestijo@gmail.com>
2022-09-25 01:58:07 +0800

    update: init day picker

---
+++ app/components/Container.tsx
@@ -0,0 +1,3 @@
+export const Container: React.FC<React.PropsWithChildren> = (props) => {
+	return <div>{props.children}</div>
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,37 +1,39 @@
+import { useState } from 'react'
+import { DayPicker } from 'react-day-picker'
+import { format } from 'date-fns'
+import type { MetaFunction, LinksFunction } from '@remix-run/node'
+import dayPickerStyles from 'react-day-picker/dist/style.css'
+import root from '~/styles/day-picker.css'
+import { Container } from '~/components/Container'
+
+export const meta: MetaFunction = () => ({
+	title: 'git-think',
+})
+
+export const links: LinksFunction = () => [
+	{ rel: 'stylesheet', href: dayPickerStyles as unknown as string },
+	{ rel: 'stylesheet', href: root },
+]
+
 export default function Index() {
+	const [selected, setSelected] = useState<Date>()
+
+	let footer = <p>Please pick a day.</p>
+	if (selected) {
+		footer = <p>You picked {format(selected, 'PP')}.</p>
+	}
+
 	return (
-		<div
-			style={{
-				fontFamily: 'system-ui, sans-serif',
-				lineHeight: '1.4',
-			}}
-		>
-			<h1>Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
+		<Container>
+			<DayPicker
+				mode="single"
+				selected={selected}
+				onSelect={setSelected}
+				footer={footer}
+				showOutsideDays
+				fixedWeeks
+				showWeekNumber
+			/>
+		</Container>
 	)
 }
